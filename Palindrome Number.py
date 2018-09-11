89a5b1107b98e6d771812f9002b656d6eaf99ce6
Noah Sun <noah22sun@gmail.com>
2018-09-11 14:47:49 +0200

    longest palindromic substring

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -17,3 +17,21 @@ class Solution:
 
 # Input: 121
 # Output: true
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if len(s) < 2:
+            return s
+        palins = []
+        for i in range(len(s)):
+            for j in range(i,len(s)+1):
+                temp = s[i:j]
+                if temp == temp[::-1]:
+                    if len(temp)>len(palins):
+                        palins = temp
+        return palins
+# Input: "babad"
+# Output: "bab"
+    
