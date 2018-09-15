eb7d10dcf7836be4f3246b068768ce100588daae
Noah Sun <noah22sun@gmail.com>
2018-09-15 18:06:33 +0200

    length of last word

---
+++ Length of Last Word.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+        return len(s.strip().split(' ')[-1])
