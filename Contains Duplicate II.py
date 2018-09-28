e914136b66861d6f9362acbfea742fb1c9641fa9
Noah Sun <noah22sun@gmail.com>
2018-09-28 10:00:49 +0200

    Contains Duplicate II

---
+++ Contains Duplicate II.py
@@ -0,0 +1,16 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        dic = {}
+        for i in range(len(nums)):
+            if nums[i] in dic and i - dic[nums[i]] <= k:
+                return True
+            dic[nums[i]] = i
+        return False
+# Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+# Input: nums = [1,2,3,1], k = 3
+# Output: true
