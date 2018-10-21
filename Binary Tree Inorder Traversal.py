ec8cc2e997b197f8a21b5fe164bab143485dc607
Noah Sun <noah22sun@gmail.com>
2018-10-21 12:36:33 +0200

    Binary Tree Inorder Traversal

--- Binary Tree Inorder Traversal.py
+++ Binary Tree Inorder Traversal.py
@@ -6,7 +6,7 @@
 #         self.right = None
 
 class Solution(object):
-    def inorderTraversal(self, root):
+    def inorderTraversal(self, root):# Recursion, Time O(n), worst space O(n) depends on num of nodes
         """
         :type root: TreeNode
         :rtype: List[int]
@@ -30,3 +30,25 @@ class Solution(object):
 #    3
 
 # Output: [1,3,2]
+
+
+
+
+
+    # def inorderTraversal(self, root): #Iterative
+    #     """
+    #     :type root: TreeNode
+    #     :rtype: List[int]
+    #     """
+    #     result = []
+    #     stack = []
+    #     #first find the end of left while putting left roots in stack, append left to result, append root to result, append right to stack, if leaf pop right and append right to result, pop parent root.....
+    #     #Time O(n) Space O(n)
+    #     while stack or root:
+    #         while root:
+    #             stack.append(root)
+    #             root = root.left
+    #         node = stack.pop()
+    #         result.append(node.val)
+    #         root = node.right
+    #     return result
