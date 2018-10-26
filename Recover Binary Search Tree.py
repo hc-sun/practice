e33593706fa5d4787ef871b2949cf0536d7b9202
Noah Sun <noah22sun@gmail.com>
2018-10-26 17:38:28 +0200

    Recover Binary Search Tree

--- Recover Binary Search Tree.py
+++ Recover Binary Search Tree.py
@@ -6,7 +6,7 @@
 #         self.right = None
 
 class Solution(object):
-    def recoverTree(self, root):
+    def recoverTree(self, root):#O(n) Space
         """
         :type root: TreeNode
         :rtype: void Do not return anything, modify root in-place instead.
@@ -25,6 +25,49 @@ class Solution(object):
         self.swap(node.left, temp)
         node.val = temp.pop(0)
         self.swap(node.right, temp)
+
+    # Morris Traversal O(1) Space
+    # 1. Initialize current as root 
+    # 2. While current is not NULL
+    #     If current does not have left child
+    #         a) Print current’s data
+    #         b) Go to the right, i.e., current = current->right
+    #     Else
+    #         a) Make current as right child of the rightmost 
+    #             node in current's left subtree
+    #         b) Go to this left child, i.e., current = current->left
+
+    # def recoverTree(self, root):
+    #     """
+    #     :type root: TreeNode
+    #     :rtype: void Do not return anything, modify root in-place instead.
+    #     """
+    #     curr, first, second, predec = root, None, None, None
+    #     while curr:
+    #         if not curr.left:
+    #             if predec and curr.val < predec.val:
+    #                 if not first:
+    #                     first = predec
+    #                 second = curr
+    #             predec = curr
+    #             curr = curr.right
+    #         else:
+    #             pre = curr.left
+    #             while pre.right and pre.right != curr:
+    #                 pre = pre.right
+    #             if not pre.right:
+    #                 pre.right = curr
+    #                 curr = curr.left
+    #             else:
+    #                 if predec and curr.val < predec.val:
+    #                     if not first:
+    #                         first = predec
+    #                     second = curr
+    #                 predec = curr
+    #                 pre.right = None
+    #                 curr = curr.right
+    #     first.val, second.val = second.val, first.val
+                
 
 # Two elements of a binary search tree (BST) are swapped by mistake.
 # Recover the tree without changing its structure.
