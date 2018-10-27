382ed4a53e7758ebf510a79f57c165a080ad6621
Noah Sun <noah22sun@gmail.com>
2018-10-27 16:24:01 +0200

    Maximum Size Subarray Sum Equals k

---
+++ Maximum Size Subarray Sum Equals k.py
@@ -0,0 +1,17 @@
+def max_array_size(nums, k): #O(n) Time
+    max_size = 0
+    for i in range(len(nums)):
+        for j in range(i, len(nums)):
+            if sum(nums[i:j]) == k:
+                max_size = max(max_size, j-i)
+    return max_size
+
+# Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn't one, return 0 instead.
+# Note:
+# The sum of the entire nums array is guaranteed to fit within the 32-bit signed integer range.
+# Input: nums = [1, -1, 5, -2, 3], k = 3
+# Output: 4 
+# Explanation: The subarray [1, -1, 5, -2] sums to 3 and is the longest.
+# Input: nums = [-2, -1, 2, 1], k = 1
+# Output: 2 
+# Explanation: The subarray [-1, 2] sums to 1 and is the longest.
